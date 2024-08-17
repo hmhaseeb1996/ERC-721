618ce387dfb8e3d7f1d60a4fe957249838aa258c
hmhaseeb1996 haseeb <hmhaseeb1996@gmail.com>
2024-08-17 19:13:53 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("ERC","er")
+        ERC721("erc-721","er")
         Ownable(initialOwner)
     {}
 
